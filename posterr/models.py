ed3a404447ae9505d309ff78553d5deb5a838cdc
Pierre Dechery <pierre.dechery@gmail.com>
2022-03-07 18:02:18 -0300

    - Defining Models - Creating migrations - Creating Fixtures

---
+++ posterr/models.py
@@ -0,0 +1,56 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser, User
+from django.conf import settings
+
+'''
+Modelos:
+
+Usuário
+  - Many to many self
+  - id
+
+Post
+  - id usuario
+  - conteudo
+  - data
+
+Repost
+  - id post
+  - id usuario
+  - data
+
+Quote
+ - id post
+ - conteudo (Quote)
+ - id user
+ - data
+'''
+
+class User(AbstractUser):
+  name = models.CharField(max_length=14, unique=True)
+  friends = models.ManyToManyField(settings.AUTH_USER_MODEL, blank=True)
+  creation_date = models.DateField(auto_now_add=True)
+
+  def save(self, *args, **kwargs):
+    if not self.username:
+      import random
+      self.username = 'poster_'+str(random.randint(10,100))
+    super().save(*args, **kwargs)
+
+
+class Post(models.Model):
+  poster = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='posts', on_delete=models.CASCADE)
+  content = models.CharField(max_length=777)
+  date = models.DateField(auto_now_add=True)
+
+class Repost(models.Model):
+  post = models.ForeignKey(Post, related_name='reposts', on_delete=models.CASCADE)
+  reposter = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+  date = models.DateField(auto_now_add=True)
+
+class Quote(models.Model):
+  post = models.ForeignKey(Post, related_name='quotes', on_delete=models.CASCADE)
+  quote = models.CharField(max_length=200)
+  quoter = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+  date = models.DateField(auto_now_add=True)
+
